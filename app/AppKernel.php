42694bc3c5d5246fcf8c273440773ff90858cae0
Victor Passapera <vpassapera@gmail.com>
2017-08-01 09:46:12 +0000

    Need fos, etc.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
-            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+	    new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
+	    new AppBundle\AppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
